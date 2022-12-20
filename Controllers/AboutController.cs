ea5b41fd4c35ae7579f8d25b30942888758a4822
yakovsmonarch <medvejonok07@gmail.com>
2022-12-20 19:27:07 +0800

    Добавьте файлы проекта.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CmdProfect.Controllers
+{
+    [Route("wof/about")]
+    public class AboutController : Controller
+    {
+        [Route("show")]
+        public string Index(string id, string param)
+        {
+            return $"About id = {id} params = {param}";
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CmdProfect.Controllers
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        private Person _person = new Person() { FirstName = "Yakov", LastName = "Sereb"};
+
+        public IActionResult Index()
+        {
+            return View(_person);
+        }
+
+        public IActionResult Show(int id)
+        {
+            if (id < 0)
+                return BadRequest("Id cannot be negative");
+            if (id == 10)
+                return Content("Hello Show");
+            return this.NotFound();
+        }
+    }
+}
